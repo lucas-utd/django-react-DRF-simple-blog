90426b4e6a368c980a667175583ce4a15f939bff
lucas-utd <lucaschentao@gmail.com>
2021-01-03 22:06:47 -0600

    Finish the basci structure.

---
+++ frontend/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const routing = (
+  <BrowserRouter>
+    <React.StrictMode>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+      <Footer />
+    </React.StrictMode>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
